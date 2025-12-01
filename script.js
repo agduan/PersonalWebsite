72e4a087d02d7c2e449bbd058bc1e72b1f4ea0e4
agduan <alexandra.duan24@gmail.com>
2025-12-01 02:10:38 -0500

    Add sidebar back (by popular request)

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// Last login time display
+(function() {
+    const loginElement = document.getElementById('last-login-time');
+    if (loginElement) {
+        const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        const now = new Date();
+        const formatted = days[now.getDay()] + ' ' + months[now.getMonth()] + ' ' + now.getDate() + ' ' + 
+            now.getHours().toString().padStart(2, '0') + ':' + 
+            now.getMinutes().toString().padStart(2, '0') + ':' + 
+            now.getSeconds().toString().padStart(2, '0');
+        loginElement.textContent = formatted;
+    }
+})();
+
+// Carousel functionality
 document.querySelectorAll('.gallery-item.carousel').forEach(carousel => {
     const images = carousel.querySelectorAll('.carousel-images img');
     const indexDisplay = carousel.querySelector('.carousel-index');
